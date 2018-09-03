584e0c57a78ba8ada078b689eaaab50cb9d50f27
Steve Flynn <sflynn@flexera.com>
2018-09-03 14:15:41 +0100

    Added rand and time

--- hello.go
+++ hello.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt" 
+	"time"
+	"math/rand"
+)
 
 func main() {
 	fmt.Printf("hello, world\n")
+	fmt.Println("The current time is: ", time.Now())
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	fmt.Println("My favorite number is", r1.Intn(100))
 }
