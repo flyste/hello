6eb6a90f0fb21a6c0168bb27781fd0e91dff4bab
Steve Flynn <sflynn@flexera.com>
2018-09-04 11:24:25 +0100

    Added rand and time, now server

--- hello.go
+++ hello.go
@@ -4,10 +4,22 @@ import (
 	"fmt" 
 	"time"
 	"math/rand"
+	"os"
+	"net/http"
 )
 
 func main() {
-	fmt.Printf("hello, world\n")
+
+	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "The current time is: %s\n", time.Now())
+		s1 := rand.NewSource(time.Now().UnixNano())
+		r1 := rand.New(s1)
+		fmt.Fprintf(w, "My favorite number is %d", r1.Intn(100))
+	})
+
+	http.ListenAndServe(":80", nil)
+	
+	fmt.Printf("hello, ", os.Args[2])
 	fmt.Println("The current time is: ", time.Now())
 	s1 := rand.NewSource(time.Now().UnixNano())
 	r1 := rand.New(s1)
