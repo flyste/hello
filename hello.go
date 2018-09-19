fd383ef3097736ffb299277bf4e37c6a0387eddc
Steve Flynn <sflynn@flexera.com>
2018-09-19 15:27:06 +0100

    Added Used count

--- hello.go
+++ hello.go
@@ -128,6 +128,7 @@ func Features(w http.ResponseWriter, r *http.Request){
 		Name string
 		Version string
 		Count string
+		Used int
 		}
 		
 	
@@ -186,6 +187,7 @@ func Features(w http.ResponseWriter, r *http.Request){
 			featureslist[i].Name = string(responseObject[i].FeatureName)
 			featureslist[i].Version = string(responseObject[i].FeatureVersion)
 			featureslist[i].Count = string(responseObject[i].FeatureCount)
+			featureslist[i].Used = responseObject[i].Used
 			}
 	
 
